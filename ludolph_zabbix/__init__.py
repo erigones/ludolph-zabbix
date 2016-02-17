993a8df278507cc41bd86f265313deb079a7bbdf
Daniel Kontsek <daniel@kontsek.sk>
2016-02-17 16:16:51 +0100

    Going to release version 1.3

--- ludolph_zabbix/__init__.py
+++ ludolph_zabbix/__init__.py
@@ -1,8 +1,8 @@
 """
 This file is part of Ludolph: Zabbix API plugin
-Copyright (C) 2015 Erigones, s. r. o.
+Copyright (C) 2015-2016 Erigones, s. r. o.
 
 See the LICENSE file for copying permission.
 """
 
-__version__ = '1.2'
+__version__ = '1.3'
