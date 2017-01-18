50a9e86cc620f2cea7100ce7b375e0bed1eb4d13
Daniel Kontsek <daniel@kontsek.sk>
2017-01-18 06:42:24 +0100

    Starting to work on new version - 1.7

--- ludolph_zabbix/__init__.py
+++ ludolph_zabbix/__init__.py
@@ -1,8 +1,8 @@
 """
 This file is part of Ludolph: Zabbix API plugin
-Copyright (C) 2015-2016 Erigones, s. r. o.
+Copyright (C) 2015-2017 Erigones, s. r. o.
 
 See the LICENSE file for copying permission.
 """
 
-__version__ = '1.6'
+__version__ = '1.7'
--- ludolph_zabbix/zapi.py
+++ ludolph_zabbix/zapi.py
@@ -1,6 +1,6 @@
 """
 This file is part of Ludolph: Zabbix API plugin
-Copyright (C) 2015-2016 Erigones, s. r. o.
+Copyright (C) 2015-2017 Erigones, s. r. o.
 
 See the LICENSE file for copying permission.
 """
